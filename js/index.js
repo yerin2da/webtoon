4d482ca436e8de02816b5227c8a50cfe5765f6b0
yerin <byl06@naver.com>
2025-08-27 16:43:09 +0900

    사운드 테스트

--- js/index.js
+++ js/index.js
@@ -19,27 +19,44 @@ $(document).on('click', '#view_setting .view_setting_close_btn', function () {
     $('#view_setting').fadeOut(300);//0.3초
 });
 
-
-// Swiper 초기화
 $("#slide").load("slide.html", function () {
     slide_reset('');//기본 슬라이드
     intro_animation();
     // 초기 슬라이드 소리 재생
     // if (!isMuted) sound[0].play();
-    initSoundBtn();
+    // initSoundBtn();
 });
-function initSoundBtn() {
-    $(document).on('click', '#sound_btn', function () {
-        isMuted = !isMuted;
-        if (isMuted) {
-            sound.forEach(s => s.volume(0));
-            $('#sound_btn i').attr('class', 'fa-solid fa-volume-xmark');
-        } else {
-            sound.forEach(s => s.volume(0.5));
-            $('#sound_btn i').attr('class', 'fa-solid fa-volume-high');
-        }
-    });
-}
+// $(document).on('click', '#sound_btn', function () {
+//     initSoundBtn()
+// });
+//
+// function initSoundBtn() {
+//     isMuted = !isMuted;
+//     if (isMuted) {
+//         sound.forEach(s => s.volume(0));
+//         $('#sound_btn i').attr('class', 'fa-solid fa-volume-xmark');
+//     } else {
+//         sound.forEach(s => s.volume(0.5));
+//         $('#sound_btn i').attr('class', 'fa-solid fa-volume-high');
+//     }
+// }
+let soundStarted = false; // 모바일 초기 재생 체크
+
+$(document).on('click touchstart', '#sound_btn', function () {
+    if (!soundStarted) {
+        // 첫 클릭 시 모든 사운드 한번씩 play & 바로 stop
+        sound.forEach(s => {
+            s.play();
+            s.stop();
+        });
+        soundStarted = true;
+    }
+
+    // 토글 음소거
+    isMuted = !isMuted;
+    sound.forEach(s => s.volume(isMuted ? 0 : 0.5));
+    $('#sound_btn i').attr('class', isMuted ? 'fa-solid fa-volume-xmark' : 'fa-solid fa-volume-high');
+});
 
 function intro_animation() {
     const intro = $('.lottie_img_wrap');
@@ -60,40 +77,6 @@ $('.go_first').click(function () {
     swiper.slideToLoop(0);//슬라이드 맨 처음으로 이동
 });
 
-//넘김효과 선택
-$(document).on('change', 'input[name="effect-input"]', function () {
-    // 현재 선택된 것만 checked
-    $('input[name="effect-input"]').prop('checked', false);
-    $(this).prop('checked', true);
-
-    let selectedEffect = '';//넘김 효과
-    if (this.id === 'fade-input') {//페이드
-        selectedEffect = 'fade';
-
-        $("#slide").load("slide.html", function () {
-            slide_reset(selectedEffect);
-            $("#flip").hide();
-            $("#slide").show();
-            $('input[name="fade-input"]').prop('checked', true);
-        });
-
-    } else if (this.id === 'flip-input') {// 플립
-        $("#flip").load("slide2.html", function () {
-            $("#slide").hide();
-            $("#flip").show();
-            flip_slide();
-        });
-
-    } else {
-        selectedEffect = ''; // 기본 슬라이드
-        // Swiper 초기화
-        $("#slide").load("slide.html", function () {
-            slide_reset(selectedEffect);
-            $("#flip").hide();
-            $("#slide").show();
-        });
-    }
-})
 
 // 슬라이드별 사운드 미리 정의
 const sound = [
@@ -106,15 +89,21 @@ const sound = [
     new Howl({ src: ['./img/sound/cute-bgm.m4a'], volume: 0.5 }),//7
 ];
 
-//슬라이드 초기설정
-function slide_reset(selectedEffect) {
-    const progressCircle = document.querySelector(".autoplay-progress svg");
-    const progressContent = document.querySelector(".autoplay-progress span");
+
+
+// Swiper 초기화 함수 (중복 제거)
+function slide_reset(selectedEffect = '') {
+    // 기존 swiper destroy
+    if (swiper) swiper.destroy(true, true);
 
     swiper = new Swiper(".mySwiper", {
         spaceBetween: 30,
-        effect: selectedEffect, // 선택된 효과 적용
+        effect: selectedEffect,
         centeredSlides: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
         pagination: {
             el: ".swiper-pagination-bullets",
             clickable: true
@@ -122,78 +111,41 @@ function slide_reset(selectedEffect) {
         scrollbar: {
             el: ".swiper-scrollbar"
         },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-        },
-        // 마우스휠
-        mousewheel: {
-            invert: false,    // false: 아래로 → 다음 슬라이드
-            sensitivity: 1,   // 감도
-        },
-        // 키보드
-        keyboard: {
-            enabled: true,    // 키보드 이벤트 활성화
-            onlyInViewport: true, // 화면 안에서만 작동
-        },
+        mousewheel: { invert: false, sensitivity: 1 },
+        keyboard: { enabled: true, onlyInViewport: true },
+        creativeEffect: selectedEffect === 'creative' ? {
+            prev: { shadow: false, translate: ["-100%", 0, -400], rotate: [0, -180, 0] },
+            next: { shadow: false, rotate: [0, 0, 0] }
+        } : {},
         on: {
             slideChange: function () {
-                let index = this.realIndex; // 현재 슬라이드 인덱스
-                let progress = ((this.realIndex + 1) / this.slides.length) * 100;
-                document.querySelector(".swiper-pagination-progress").style.width = progress + "%";
+                const index = this.realIndex;
+                const progressBar = document.querySelector(".swiper-pagination-progress");
+                if (progressBar) progressBar.style.width = ((index + 1) / this.slides.length * 100) + "%";
 
-                sound.forEach(s => s.stop()); // 모든 소리 중지
-                if (sound[index]) {
-                    sound[index].play();
-                }
+                sound.forEach(s => s.stop());
+                if (sound[index]) sound[index].play();
             }
         }
     });
 }
 
-function flip_slide() {
-    swiper = new Swiper(".mySwiper.perspective", {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        effect: "creative",
-        rtl: true,
-        centeredSlides: false,
-        speed: 1000,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination-bullets",
-            clickable: true,
-        },
-        mousewheel: {
-            invert: false,
-            sensitivity: 1,
-        },
-        keyboard: {
-            enabled: true,
-            onlyInViewport: true,
-        },
-        creativeEffect: {
-            prev: {
-                shadow: false,
-                translate: ["-100%", 0, -400],//x축, y축, z축
-                rotate: [0, -180, 0],
-            },
-            next: {
-                shadow: false,
-                // translate: ["-100%", 0, -400],
-                rotate: [0, 0, 0],
-            },
-        },
-        on: {
-            slideChange: function () {
-                const progressBar = document.querySelector("#flip .swiper-pagination-progress");
-                if (progressBar) {
-                    progressBar.style.width = ((this.realIndex + 1) / this.slides.length * 100) + "%";
-                }
-            },
-        }
+// 넘김효과 선택
+$(document).on('change', 'input[name="effect-input"]', function () {
+    $('input[name="effect-input"]').prop('checked', false);
+    $(this).prop('checked', true);
+
+    let selectedEffect = '';
+    let slideFile = 'slide.html';
+    if (this.id === 'fade-input') selectedEffect = 'fade';
+    else if (this.id === 'flip-input') {
+        selectedEffect = 'creative';
+        slideFile = 'slide2.html'; // flip은 별도 HTML
+    }
+
+    $("#slide").load(slideFile, function () {
+        $("#flip").hide();
+        $("#slide").show();
+        slide_reset(selectedEffect);
     });
-}
+});
